ed1d151b2f8a7ec4714d399b4fb24f3079074877
cheshirehat <cheshireinhatter@gmail.com>
2019-10-25 11:46:56 +0900

    ktkr

--- test.sh
+++ test.sh
@@ -8,9 +8,9 @@ git remote set-url origin https://cheshirehat:${GITHUB_TOKEN}@github.com/cheshir
 git branch -a
 
 for remote_branch in `git branch -a | grep remotes | grep -v HEAD | grep -v master | grep -v develop`; do
-  # echo "$(echo -n "$remote_branch" | cut -d/ -f3-)"
-  b="$(echo -n "$remote_branch" | cut -d/ -f3-)"
-  `git checkout "$(echo -n "$remote_branch" | cut -d/ -f3)"`
+  echo "$(echo -n "$remote_branch" | cut -d/ -f3-)"
+  # b="$(echo -n "$remote_branch" | cut -d/ -f3-)"
+  `git checkout "$(echo -n "$remote_branch" | cut -d/ -f3-)"`
   git branch
   git merge origin/master
   git push
